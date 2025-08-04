93f4225a198ff98d4227da669338e0a944088034
CarloMart88 <carlo.martino1988@gmail.com>
2025-08-04 14:59:47 +0200

    feat: controller done

--- controller/postsController.js
+++ controller/postsController.js
@@ -1,40 +1,46 @@
+const posts = require('../dataposts.js')
+
 
 // index // per vedere tutti gli elementi 
 
-(req , res) => {
+const index = (req , res) => {
   res.json('Per visualizzare tutti gli elementi')
 }
 
 // show // per vedere tutti gli elementi 
 
-(req , res) => {
+const show = (req , res) => {
   let id = req.params.id
   res.json(`Per visualizzare tutti gli elementi con ${id}`)
 }
 
-// post // per creare un elemento nuovo
+// store // per creare un elemento nuovo
 
-(req , res) => {
+const store = (req , res) => {
   res.json('per creare un elemento nuovo')
 }
 
-// modify // per modificare totalmente un elemento con id 
+// update // per modificare totalmente un elemento con id 
 
-(req , res) => {
+const update = (req , res) => {
   let id = req.params.id
   res.json(`per modificare totalmente un elemento con ${id}`)
 }
 
-// patch // per modificare parzialmente un elemento con id 
+// modify // per modificare parzialmente un elemento con id 
 
-(req , res) => {
+const modify = (req , res) => {
   let id = req.params.id
   res.json(`per modificare parzialmente un elemento con ${id}`)
 }
 
 // remove  // per eliminare un elemento con id 
 
-(req , res) => {
+const destroy = (req , res) => {
   let id = req.params.id
   res.json(`per eliminare un elemento con ${id}`)
+}
+
+module.exports = {
+  index,show,store,modify,update,destroy
 }
