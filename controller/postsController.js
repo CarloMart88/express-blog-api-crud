9dc1efe63a7fa5550c6cc62e03cdd55e4dd72c4c
CarloMart88 <carlo.martino1988@gmail.com>
2025-08-04 15:43:42 +0200

    feat : add index a main logic and work on const show

--- controller/postsController.js
+++ controller/postsController.js
@@ -1,17 +1,43 @@
 const posts = require('../dataposts.js')
 
 
+
+// Milestone 2
+// Per iniziare, creiamo una cartella data in cui creare un file che contenga ed esporti l’array di posts che trovate in allegato. Importiamo questo file in cima al controller. ok 
+
+// Ora passiamo ad implementare le logiche delle nostre CRUD: ok 
+
+// Index dovrà restituire la lista dei post in formato JSON ok 
+
+// Show dovrà restituire un singolo post in formato JSON
+
+
+// Destroy dovrà eliminare un singolo post dalla lista, stampare nel terminale (console.log) la lista aggiornata, e rispondere con uno stato 204 e nessun contenuto.
+
+
 // index // per vedere tutti gli elementi 
 
 const index = (req , res) => {
-  res.json('Per visualizzare tutti gli elementi')
+  //facendo così già restituisco l'array
+  res.json(posts)
 }
 
 // show // per vedere tutti gli elementi 
 
 const show = (req , res) => {
-  let id = req.params.id
-  res.json(`Per visualizzare tutti gli elementi con id ${id}`)
+  const id = parseInt(req.params.id)
+
+  //uso id per trovare il singolo elemento
+  
+  const post = posts.find(elem => elem.id === id)
+ 
+  // effettuiamo un controllo su post per vedere se esiste
+  if(!post){
+    return res.status(404).res.json({ error: '404 not found '
+      ,messagge:'Post non presente'})
+  }
+
+  res.json(post)
 }
 
 // store // per creare un elemento nuovo
@@ -44,3 +70,6 @@ const destroy = (req , res) => {
 module.exports = {
   index,show,store,modify,update,destroy
 }
+
+
+
