7931dadc2265a0d697ba4eaf4bcc114828c38f99
CarloMart88 <carlo.martino1988@gmail.com>
2025-08-04 14:32:40 +0200

    feat: prepare app.js and it first path and add router , controller and dataposts

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log ('ciao')
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+
+app.use(express.static ('public'))
+
+
+app.get("/" , (req , res) =>{
+  res.send('Server per i miei post')
+})
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server in ascolto sulla porta ${port}`)
+})
