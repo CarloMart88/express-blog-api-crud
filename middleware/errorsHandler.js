089af7e952f88fd9393a5407a3705df59f7ad76c
CarloMart88 <carlo.martino1988@gmail.com>
2025-08-06 15:04:16 +0200

    feat: errorshandler.js logic

--- middleware/errorsHandler.js
+++ middleware/errorsHandler.js
@@ -1,1 +1,11 @@
-///
+// definizione di middleware 
+
+const errorsHandler = (err , req , res , next) => {
+
+
+  res.status(500).json({
+    errore : err.message
+  })
+}
+
+module.exports = errorsHandler
